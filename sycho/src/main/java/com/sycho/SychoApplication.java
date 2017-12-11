f31656d0ff075bf48cc2a3006e3a1a3c36bf2060
s10cho <tmdduf22>
2017-12-11 20:39:56 +0900

    react

---
+++ sycho/src/main/java/com/sycho/SychoApplication.java
@@ -0,0 +1,14 @@
+package com.sycho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SychoApplication
+{
+	//
+	public static void main(String[] args) {
+		//
+		SpringApplication.run(SychoApplication.class, args);
+	}
+}
